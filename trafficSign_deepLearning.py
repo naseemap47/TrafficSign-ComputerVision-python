3261d0c631c3f614b9992b625cbb7ab46ec9b5a2
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-25 14:41:51 +0530

    test data ordered

--- trafficSign_deepLearning.py
+++ trafficSign_deepLearning.py
@@ -3,13 +3,19 @@ import glob
 import shutil
 from sklearn.model_selection import train_test_split
 import shutil
-from my_utils import split_data
+from my_utils import split_data, order_test_data
 
 
 if __name__=='__main__':
 
-    path_to_data = '/home/naseem/My Project/TrafficSign-ComputerVision-python/train'
-    path_to_save_train = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/train'
-    path_to_save_val = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/val'
+    if False:
+        path_to_data = '/home/naseem/My Project/TrafficSign-ComputerVision-python/train'
+        path_to_save_train = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/train'
+        path_to_save_val = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/val'
+        split_data(path_to_data, path_to_save_train, path_to_save_val)
+    
+    path_to_data = '/home/naseem/My Project/TrafficSign-ComputerVision-python/test'
+    path_to_csv = '/home/naseem/My Project/TrafficSign-ComputerVision-python/Test.csv'
 
-    split_data(path_to_data, path_to_save_train, path_to_save_val)
+    order_test_data(path_to_csv, path_to_data)
+    
