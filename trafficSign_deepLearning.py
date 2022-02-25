b04ca0e56d1cd9b423e426345aff4048569a0e9c
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-25 16:22:29 +0530

    writen generator function

--- trafficSign_deepLearning.py
+++ trafficSign_deepLearning.py
@@ -3,19 +3,18 @@ import glob
 import shutil
 from sklearn.model_selection import train_test_split
 import shutil
-from my_utils import split_data, order_test_data
+from my_utils import create_generators
 
 
 if __name__=='__main__':
 
-    if False:
-        path_to_data = '/home/naseem/My Project/TrafficSign-ComputerVision-python/train'
-        path_to_save_train = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/train'
-        path_to_save_val = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/val'
-        split_data(path_to_data, path_to_save_train, path_to_save_val)
-    
-    path_to_data = '/home/naseem/My Project/TrafficSign-ComputerVision-python/test'
-    path_to_csv = '/home/naseem/My Project/TrafficSign-ComputerVision-python/Test.csv'
+    path_to_train = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/train'
+    path_to_val = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/val'
+    path_to_test = '/home/naseem/My Project/TrafficSign-ComputerVision-python/test'
 
-    order_test_data(path_to_csv, path_to_data)
-    
+    train_generators, val_generators, test_generators = create_generators(
+        batch_size=32,
+        path_to_train_data=path_to_train,
+        path_to_val_data=path_to_val,
+        path_to_test_data=path_to_test
+    )
