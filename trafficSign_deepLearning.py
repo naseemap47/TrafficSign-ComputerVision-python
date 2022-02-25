71017a004a47596ca4d60e11c76919c1fdd268d0
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-25 12:19:42 +0530

    fixed error with train and val split size

--- trafficSign_deepLearning.py
+++ trafficSign_deepLearning.py
@@ -13,7 +13,7 @@ def split_data(path_to_data, path_to_save_train, path_to_save_val, split_size=0.
         full_path = os.path.join(path_to_data, folder)
         path_to_images = glob.glob(os.path.join(full_path, '*png'))
 
-        x_train, x_val = train_test_split(path_to_images, train_size=split_size)
+        x_train, x_val = train_test_split(path_to_images, test_size=split_size)
 
         for x in x_train:
             path_to_folder = os.path.join(path_to_save_train, folder)
@@ -31,4 +31,10 @@ def split_data(path_to_data, path_to_save_train, path_to_save_val, split_size=0.
 
             shutil.copy(x, path_to_folder)
 
+if __name__=='__main__':
 
+    path_to_data = '/home/naseem/My Project/TrafficSign-ComputerVision-python/train'
+    path_to_save_train = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/train'
+    path_to_save_val = '/home/naseem/My Project/TrafficSign-ComputerVision-python/training_data/val'
+
+    split_data(path_to_data, path_to_save_train, path_to_save_val)
