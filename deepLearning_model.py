4f31862d7b4bcbf6d6997c7bbd8d60e7c6c2e911
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-25 15:40:03 +0530

    created deepLearning model for trafficSign boards

---
+++ deepLearning_model.py
@@ -0,0 +1,23 @@
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+def trafficSign_model(no_classes):
+    my_input = layers.Input(shape=(70,70,3))
+    
+    x = layers.Conv2D(32, (3,3), activation='relu')(my_input)
+    x = layers.MaxPool2D()(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv2D(64, (3,3), activation='relu')(x)
+    x = layers.MaxPool2D()(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Conv2D(128, (3,3), activation='relu')(x)
+    x = layers.MaxPool2D()(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.GlobalAvgPool2D()(x)
+    x = layers.Dense(128, activation='relu')(x)
+    x = layers.Dense(no_classes, activation='softmax')(x)
+    
+    return Model(inputs=my_input, outputs=x)
