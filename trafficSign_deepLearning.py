9b77e17cba0d21331c72ef61efba61a3b402250a
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-25 16:40:24 +0530

    model, compile and fit

--- trafficSign_deepLearning.py
+++ trafficSign_deepLearning.py
@@ -1,10 +1,5 @@
-import os
-import glob
-import shutil
-from sklearn.model_selection import train_test_split
-import shutil
 from my_utils import create_generators
-
+from deepLearning_model import trafficSign_model
 
 if __name__=='__main__':
 
@@ -17,4 +12,18 @@ if __name__=='__main__':
         path_to_train_data=path_to_train,
         path_to_val_data=path_to_val,
         path_to_test_data=path_to_test
+    )
+    number_classes = train_generators.num_classes
+
+    model = trafficSign_model(no_classes=number_classes)
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    model.fit(
+        train_generators,
+        batch_size=32,
+        epochs=2,
+        validation_data=val_generators,
     )
